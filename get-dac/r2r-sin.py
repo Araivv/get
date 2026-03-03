abc84dbf40689129e03c03858ea40b3ae85e2d7f
Muslim <tureshev.mm@phystech.edu>
2026-03-03 13:37:18 +0300

    second try

--- get-dac/r2r-sin.py
+++ get-dac/r2r-sin.py
@@ -1,25 +1,31 @@
 import r2r_dac as r2r
 import signal_generator as sg
-import time
 
-amplitude = 3.2
+
+amplitude = 3.0
 signal_frequency = 10
 sampling_frequency = 1000
+sampling_period = 1.0 / sampling_frequency
 
 if __name__ == "__main__":
     try:
         massive = [16, 20, 21, 25, 26, 17, 27, 22]
         dac = r2r.R2R_DAC(massive, 3.0, True)
 
+        time_counter = 0
+
         while True:
-            try:
-                times = 0
-                voltage = sg.get_sin_wave_amplitude(100, times)
-                dac.set_voltage(voltage)
-                times += 0.1
+            normalized_value = sg.get_sin_wave_amplitude(signal_frequency, time_counter)
 
-            except ValueError:
-                print("Вы ввели не число. Попробуйте ещё раз\n")
+            voltage = normalized_value * amplitude
 
+            dac.set_voltage(voltage)
+
+            time_counter += sampling_period
+
+            sg.wait_for_sampling_period(sampling_frequency)
+
+    except KeyboardInterrupt:
+        print("\nПрограмма остановлена пользователем")
     finally:
         dac.deinit()
